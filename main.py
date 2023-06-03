aad7ca562c0dc4c19eacc6d03f0325f697bbe17a
cmdr <cmdr@ryuuhou.com>
2023-06-03 18:31:26 -0500

    added enemy spawns

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 #Game initialization
 
+import random
 import pyglet
-from GameObjects import PhysicsObject
+from GameObjects import PhysicsObject, Player, Enemy
 from GameResources import *
 
 game_window = pyglet.window.Window(800,600)
@@ -15,10 +16,10 @@ main_batch = pyglet.graphics.Batch()
 score_label = pyglet.text.Label(text="Score: 0",x=10,y=575,batch=main_batch)
 
 #Create Player
-player = PhysicsObject(img=player_image,x=400,y=300,batch=main_batch)
-player.scale = 0.1
-player.radius = 25
-player.vx = 100
+player = Player(x=400,y=300,batch=main_batch)
+
+game_window.push_handlers(player)
+game_window.push_handlers(player.key_handler)
 
 #Create object list
 game_objects = [player]
@@ -35,9 +36,32 @@ def on_draw():
     main_batch.draw()
 
 def update(dt):
-    for obj in game_objects:
-        obj.update(dt)
+    for obj in reversed(game_objects):
+
+        obj.update(dt,[other_obj for other_obj in game_objects if other_obj is not obj])
+
+
+        game_objects.extend(obj.new_objects)
+        obj.new_objects = []
+
+
+        if obj.dead == True:
+            obj.delete()
+            game_objects.remove(obj)
+
+            
+
+def spawn_enemy(dt):
+    x_coord = game_window.size[0]-50
+    y_coord = random.random()*(game_window.size[1]-100)+50
+    new_enemy = Enemy(x=x_coord,y=y_coord,batch=main_batch)
+
+    new_enemy.vx = -50    
+    game_objects.append(new_enemy)
 
 pyglet.clock.schedule_interval(update,1/60.0)
+pyglet.clock.schedule_interval(spawn_enemy,2)
 pyglet.app.run()
 
+
+# %%
