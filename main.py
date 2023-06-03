646b2d4eaae40640defc0aa99ebe7c31877134fb
cmdr <cmdr@ryuuhou.com>
2023-06-02 22:58:46 -0500

    moved resources to GameResources.py

--- main.py
+++ main.py
@@ -3,25 +3,10 @@
 #Game initialization
 
 import pyglet
-
-pyglet.resource.path = ['./sprites']
-pyglet.resource.reindex()
+from GameObjects import PhysicsObject
+from GameResources import *
 
 game_window = pyglet.window.Window(800,600)
-
-
-#Load Sprites
-player_image = pyglet.resource.image("Nasa.png")
-bullet_image = pyglet.resource.image("bullet.png")
-enemy_image = pyglet.resource.image("enemy.png")
-
-def center_image(image):
-    image.anchor_x = image.width // 2
-    image.anchor_y = image.height // 2
-
-center_image(player_image)
-center_image(bullet_image)
-center_image(enemy_image)
 
 #Draw Batch
 main_batch = pyglet.graphics.Batch()
@@ -30,9 +15,13 @@ main_batch = pyglet.graphics.Batch()
 score_label = pyglet.text.Label(text="Score: 0",x=10,y=575,batch=main_batch)
 
 #Create Player
-player = pyglet.sprite.Sprite(img=player_image,x=400,y=300,batch=main_batch)
+player = PhysicsObject(img=player_image,x=400,y=300,batch=main_batch)
 player.scale = 0.1
+player.radius = 25
+player.vx = 100
 
+#Create object list
+game_objects = [player]
 
 
 #%%
@@ -45,6 +34,10 @@ def on_draw():
 
     main_batch.draw()
 
+def update(dt):
+    for obj in game_objects:
+        obj.update(dt)
 
-
+pyglet.clock.schedule_interval(update,1/60.0)
 pyglet.app.run()
+
